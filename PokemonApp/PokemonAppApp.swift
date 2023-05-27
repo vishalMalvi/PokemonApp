b0c8690e29aee5c9ad8aea424caa1651f7c29ccb
visha7malvi <vishal7malvi@gmail.com>
2023-05-28 02:06:13 +0530

    Initial commit

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Vishal_Malvi on 28/05/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
